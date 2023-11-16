ff007bc121875c907026a28949c0fd1c765c845f
seepine <seepine@outlook.com>
2023-11-16 15:35:23 +0800

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended', // 添加 prettier 插件
+    './.eslintrc-auto-import.json'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+    tsconfigRootDir: __dirname,
+    project: ['tsconfig.json'],
+    extraFileExtensions: ['.vue']
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import AutoImport from 'unplugin-auto-import/vite'
-
+import eslint from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     uni(),
+    eslint(),
     AutoImport({
       imports: ['vue', 'uni-app'],
       eslintrc: {
